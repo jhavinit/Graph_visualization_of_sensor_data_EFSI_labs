48ea5738803151fbe5d2d1c7ac826305a3670ac8
vinit <jhavinit07@gmail.com>
2019-07-02 15:18:09 +0530

    Done the refresh internally feature in NewFolder/examples/basic-line

---
+++ New folder/examples/basic-line/code.js
@@ -0,0 +1,47 @@
+$.getJSON('http://localhost/blog/data1.json', function (data) {
+    var arr2 = [];
+    for (var i = 0; i < data.length; i++) {
+        var k = 0;
+            var data1 = []
+            data1.push(data[i][k]);
+            k = k + 2; 
+            data1.push(data[i][k]);
+            arr2.push(data1);
+    }
+
+    Highcharts.stockChart('container', {
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'TEMPERATURE READINGS'
+        },
+
+        series: [{
+            name: 'Temperature',
+            data: data,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
+
+     Highcharts.stockChart('container1', {
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'HUMIDITY READINGS'
+        },
+
+        series: [{
+            name: 'Humidity',
+            data: arr2,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
+});
